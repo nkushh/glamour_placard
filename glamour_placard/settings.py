387120c48f9e713f3dd78c3148e59847e9f9685a
nkushh <mugunapiero@gmail.com>
2017-09-26 00:14:23 +0300

    Got rid of padding in product details in product.html file

--- glamour_placard/settings.py
+++ glamour_placard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9a@gt24=q&joq=a6)6l*bsq-mk957f%4^gf6!afp(&z*og+bvy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'glamour.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'glamour.pythonanywhere.com']
 
 
 # Application definition
