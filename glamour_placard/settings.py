fcfd25947100ad45b4bffba2184e430129ae99ff
nkushh <mugunapiero@gmail.com>
2017-09-25 22:56:29 +0300

    Second commit with changes to allowed hosts in settings.py file

--- glamour_placard/settings.py
+++ glamour_placard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9a@gt24=q&joq=a6)6l*bsq-mk957f%4^gf6!afp(&z*og+bvy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'http://glamour.pythonanywhere.com']
 
 
 # Application definition
