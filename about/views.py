58abba85f7fc12bc48e6988e0c946080bc4ecf52
nkushh <mugunapiero@gmail.com>
2017-10-10 17:56:57 +0300

    Changed home posts to be ordered by publishing date in descending order

--- about/views.py
+++ about/views.py
@@ -4,7 +4,7 @@ from store.models import Product
 
 # Create your views here.
 def home(request):
-	posts = Post.objects.all()[:4]
+	posts = Post.objects.filter(published_on__isnull=False).order_by('-published_on')[:4]
 	products = Product.objects.all()
 	context = {
 		'posts' : posts,
